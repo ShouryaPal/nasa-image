30f5b93cd88d89f3f4963c53f2f8103cf466df22
Shourya Pal <palshourya897@gmail.com>
2024-04-11 23:58:54 +0530

    Fix Bugs

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
+import type { NextAuthOptions } from "next-auth";
 import { authOptions } from "@/lib/authOptions";
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, authOptions };
+const handler = NextAuth(authOptions as NextAuthOptions);
+
+export { handler as GET, handler as POST };
